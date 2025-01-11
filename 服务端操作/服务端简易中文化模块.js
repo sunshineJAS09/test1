01782e6d8bf47e5c7d8b257144df72106a41218a
sunshine菌 <132323435+sunshineJAS09@users.noreply.github.com>
2025-01-11 16:53:28 +0800

    更新 服务端简易中文化模块.js

--- 服务端操作/服务端简易中文化模块.js
+++ 服务端操作/服务端简易中文化模块.js
@@ -1,1 +1,17 @@
-q
+const http = require('http');
+
+// 定义一个启动服务器的方法
+function 启动服务器(port, message) {
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(message + '\n');
+    });
+
+    server.listen(port, () => {
+        console.log(`服务器运行在 http://localhost:${port}/`);
+    });
+}
+
+// 导出 启动服务器 方法
+module.exports = 启动服务器;
