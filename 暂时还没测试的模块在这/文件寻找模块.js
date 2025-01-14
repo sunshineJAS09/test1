9b6ba5bb3e38deb1521c8bf433f3e8b85360c204
sunshine菌 <132323435+sunshineJAS09@users.noreply.github.com>
2025-01-14 13:01:49 +0800

    更新 文件寻找模块.js

--- 暂时还没测试的模块在这/文件寻找模块.js
+++ 暂时还没测试的模块在这/文件寻找模块.js
@@ -1,8 +1,8 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-// 定义一个查找文件的方法
-async function 查找文件(dirPath) {
+// 定义一个寻找文件的方法
+async function 寻找文件(dirPath, filter = '') {
     try {
         const files = await fs.readdir(dirPath, { withFileTypes: true });
         const filePaths = [];
@@ -10,19 +10,22 @@ async function 查找文件(dirPath) {
         for (const file of files) {
             const filePath = path.join(dirPath, file.name);
             if (file.isDirectory()) {
-                const nestedFiles = await 查找文件(filePath);
+                const nestedFiles = await 寻找文件(filePath, filter);
                 filePaths.push(...nestedFiles);
-            } else {
+            } else if (file.name.includes(filter)) {
                 filePaths.push(filePath);
             }
         }
 
-        return filePaths;
+        if (filePaths.length > 0) {
+            console.log('找到的文件路径:', filePaths);
+        } else {
+            console.log('没有找到符合条件的文件。');
+        }
     } catch (err) {
         console.error(`查找文件时发生错误: ${err.message}`);
-        return [];
     }
 }
 
-// 导出 查找文件 方法
-module.exports = { 查找文件 };
+// 导出 寻找文件 方法
+module.exports = { 寻找文件 };
